8b97179b62c5a87e1dce8f04af35ade56cc57359
moha6959 <mohamedekaden576@gmail.com>
2025-02-11 13:37:41 +0300

    initial commit

---
+++ src/main/java/dev/moha/med_dek/MedDekApplication.java
@@ -0,0 +1,13 @@
+package dev.moha.med_dek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedDekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedDekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/moha/med_dek/MedDekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moha.med_dek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedDekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
